6af340c35ee39b99a146157bef25985da4f07ff0
Matthias Ploetz <matthias_ploetz@yahoo.com>
2022-10-21 16:20:52 -0700

    Add project files.

---
+++ RandomThreadSafety/CustomRandom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomThreadSafety
+{
+    internal static class CustomRandom
+    {
+        [ThreadStatic]
+        private static Random? _local;
+
+        private static Random Instance => _local ??= new Random();
+
+        internal static int Next() => Instance.Next();
+    }
+}
---
+++ RandomThreadSafety/Program.cs
@@ -0,0 +1,28 @@
+﻿using RandomThreadSafety;
+using static System.Console;
+
+
+var rng = new Random(7);
+
+Parallel.For(0, 16, _ =>
+{
+    var numbers = new int[10_000];
+    for (var i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rng.Next();
+    }
+    var threadIssues = numbers.Count(y => y == 0);
+    WriteLine($"Thread issues: {threadIssues}");
+});
+
+
+Parallel.For(0, 16, _ =>
+{
+    var numbers = new int[10_000];
+    for (var i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = CustomRandom.Next();
+    }
+    var threadIssues = numbers.Count(y => y == 0);
+    WriteLine($"Thread issues: {threadIssues}");
+});
